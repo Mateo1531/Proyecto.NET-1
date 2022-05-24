8eb175eaec2fd2148edb165facd0802ced7c5f76
Mateo1531 <1174070@senati.pe>
2022-05-24 03:21:21 -0500

    cambios

--- views/adminCurso.php
+++ views/adminCurso.php
@@ -20,15 +20,26 @@
                         <th><input type="text" class="form-control" placeholder="Username" disabled></th>
                     </tr>
                 </thead>
-
-                <!-- <tbody>
+                <tbody>
                     <tr>
                         <td>1</td>
                         <td>Mark</td>
                         <td>Otto</td>
                         <td>@mdo</td>
                     </tr>
-                </tbody> -->
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
